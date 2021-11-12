cf3bc436bd520e67a139684353278e54305452ad
abcd102690 <abcd102690@gmail.com>
2021-11-12 11:01:49 +0900

    no message

--- src/mms/member/action/MemberAddAction.java
+++ src/mms/member/action/MemberAddAction.java
@@ -2,11 +2,14 @@ package mms.member.action;
 //7.회원등록
 import java.util.Scanner;
 
+import mms.member.vo.Member;
+
 public class MemberAddAction implements Action {
 
 	@Override
 	public void execute(Scanner sc) throws Exception {
-		
+		Member m = new Member();
+		m.getId()
 	}
 
 }
